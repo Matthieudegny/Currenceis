5d8b9ce7d3e9ce3d53995bf1b859f77f1125edef
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-09-22 20:12:38 +0200

    handle calcul and display result

--- dist/main.js
+++ dist/main.js
@@ -24,13 +24,38 @@ const yuan = {
     taux: 0.15,
 };
 const devises = [dollar, euro, livre, yuan];
-console.log(devises);
+const getDevise = (codeDevise, in_devises) => {
+    for (let devise of in_devises) {
+        if (codeDevise === devise.code) {
+            return devise;
+        }
+    }
+    return null;
+};
+const calculResultat = (in_montant, in_deviseInitValeur, in_deviseFinaleValeur) => {
+    let deviseInitObjet = getDevise(in_deviseInitValeur, devises);
+    let deviseFinaleObjet = getDevise(in_deviseFinaleValeur, devises);
+    let deviseInit;
+    if (deviseInitObjet)
+        deviseInit = deviseInitObjet;
+    else
+        throw { message: "La devise initiale n'est pas correcte" };
+    let deviseFinale;
+    if (deviseFinaleObjet)
+        deviseFinale = deviseFinaleObjet;
+    else
+        throw { message: "La devise finale n'est pas correcte" };
+    return (montant * deviseInit.taux) / deviseFinale.taux;
+};
+const afficherResultat = () => {
+    console.log("afficherResultat");
+    divResultat.innerText = "Résultat : " + calculResultat(montant, deviseInitValeur, deviseFinaleValeur);
+};
 const genererListeOptionDevise = (in_devises) => {
     let listeDeviseTxt = "";
     for (let devise of in_devises) {
         listeDeviseTxt += `<option value="${devise.code}">${devise.nom} - (${devise.symbole})</option>`;
     }
-    console.log("listeDeviseTxt", listeDeviseTxt);
     return listeDeviseTxt;
 };
 // ! === certifié non null
@@ -39,6 +64,7 @@ deviseInitSelect.innerHTML = genererListeOptionDevise(devises);
 let deviseInitValeur = deviseInitSelect.value;
 deviseInitSelect.addEventListener("change", () => {
     deviseInitValeur = deviseInitSelect.value;
+    afficherResultat();
     console.log("Valeur devise initiale : " + deviseInitValeur);
 });
 const deviseFinaleSelect = document.querySelector("#devise-finale");
@@ -46,9 +72,12 @@ deviseFinaleSelect.innerHTML = genererListeOptionDevise(devises);
 let deviseFinaleValeur = deviseFinaleSelect.value;
 deviseFinaleSelect.addEventListener("change", () => {
     deviseFinaleValeur = deviseFinaleSelect.value;
+    afficherResultat();
 });
 let montant;
 const montantInput = document.querySelector("#montant");
 montantInput.addEventListener("keyup", () => {
     montant = +montantInput.value;
+    afficherResultat();
 });
+let divResultat = document.querySelector("#resultat");
--- src/main.ts
+++ src/main.ts
@@ -34,14 +34,46 @@ const yuan: DeviseType = {
 };
 
 const devises: DeviseType[] = [dollar, euro, livre, yuan];
-console.log(devises);
+
+const getDevise = (codeDevise: string, in_devises: DeviseType[]): DeviseType | null => {
+  for (let devise of in_devises) {
+    if (codeDevise === devise.code) {
+      return devise;
+    }
+  }
+  return null;
+};
+const calculResultat = (
+  in_montant: number,
+  in_deviseInitValeur: string,
+  in_deviseFinaleValeur: string
+): number => {
+  let deviseInitObjet: unknown = getDevise(in_deviseInitValeur, devises);
+  let deviseFinaleObjet: unknown = getDevise(in_deviseFinaleValeur, devises);
+
+  let deviseInit: DeviseType;
+  //deviseInitObjet peut etre null au retour de getDevise, vu que if (deviseInitObjet) vérifie je certifie donc que
+  //deviseInitObjet est initialisé entant que DeviseType => deviseInitObjet as DeviseType
+  if (deviseInitObjet) deviseInit = deviseInitObjet as DeviseType;
+  else throw { message: "La devise initiale n'est pas correcte" };
+
+  let deviseFinale: DeviseType;
+  if (deviseFinaleObjet) deviseFinale = deviseFinaleObjet as DeviseType;
+  else throw { message: "La devise finale n'est pas correcte" };
+
+  return (montant * deviseInit.taux) / deviseFinale.taux;
+};
+
+const afficherResultat = () => {
+  console.log("afficherResultat");
+  divResultat.innerText = "Résultat : " + calculResultat(montant, deviseInitValeur, deviseFinaleValeur);
+};
 
 const genererListeOptionDevise = (in_devises: DeviseType[]): string => {
   let listeDeviseTxt = "";
   for (let devise of in_devises) {
     listeDeviseTxt += `<option value="${devise.code}">${devise.nom} - (${devise.symbole})</option>`;
   }
-  console.log("listeDeviseTxt", listeDeviseTxt);
   return listeDeviseTxt;
 };
 
@@ -51,6 +83,7 @@ deviseInitSelect.innerHTML = genererListeOptionDevise(devises);
 let deviseInitValeur = deviseInitSelect.value;
 deviseInitSelect.addEventListener("change", () => {
   deviseInitValeur = deviseInitSelect.value;
+  afficherResultat();
   console.log("Valeur devise initiale : " + deviseInitValeur);
 });
 
@@ -59,10 +92,14 @@ deviseFinaleSelect.innerHTML = genererListeOptionDevise(devises);
 let deviseFinaleValeur = deviseFinaleSelect.value;
 deviseFinaleSelect.addEventListener("change", () => {
   deviseFinaleValeur = deviseFinaleSelect.value;
+  afficherResultat();
 });
 
-let montant: number;
+let montant: number = 0;
 const montantInput = document.querySelector("#montant")! as HTMLInputElement;
 montantInput.addEventListener("keyup", () => {
   montant = +montantInput.value;
+  afficherResultat();
 });
+
+let divResultat = document.querySelector("#resultat")! as HTMLDivElement;
